fdce6a00f1e6184e3c53c47e801db25ccceec355
Filip Razvan <filip.razvan@agilefreaks.com>
2024-10-13 18:19:26 +0300

    Add default use case response

--- app/utils/use_case.py
+++ app/utils/use_case.py
@@ -1,10 +1,22 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+
 class UseCase:
     steps = []
 
     def perform(self, context):
+        response = self._iterate_through_steps(context)
+
+        return response if response is not None else self._default_response()
+
+    def _iterate_through_steps(self, context):
         for step_class in self.steps:
             step = step_class()
             result = step.perform(context)
 
             if result is not None:
                 return result
+
+    def _default_response(self):
+        return Response(status=status.HTTP_200_OK)
